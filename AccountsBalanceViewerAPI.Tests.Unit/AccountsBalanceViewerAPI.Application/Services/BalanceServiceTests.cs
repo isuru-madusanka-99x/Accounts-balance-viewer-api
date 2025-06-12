1eca13dea3164a8cfb1a4fc544aeadf0d28decf4
IsuruMadusanka-99x <isurum@99x.io>
2025-06-13 00:48:54 +0530

    Add unit test

--- AccountsBalanceViewerAPI.Tests.Unit/AccountsBalanceViewerAPI.Application/Services/BalanceServiceTests.cs
+++ AccountsBalanceViewerAPI.Tests.Unit/AccountsBalanceViewerAPI.Application/Services/BalanceServiceTests.cs
@@ -3,11 +3,8 @@ using AccountsBalanceViewerAPI.Application.Services.Balances;
 using AccountsBalanceViewerAPI.Domain.Models;
 using AccountsBalanceViewerAPI.Infrastructure.Interfaces;
 using FakeItEasy;
+using Microsoft.EntityFrameworkCore.Query;
 using Shouldly;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace AccountsBalanceViewerAPI.Tests.Unit.AccountsBalanceViewerAPI.Application.Services.Balances;
@@ -17,7 +14,6 @@ public class BalanceServiceTests
     [Fact]
     public async Task GetBalancesByPeriodAsync_ReturnsGroupedBalances()
     {
-        /*
         // Arrange
         var fakeUow = A.Fake<IUnitOfWork>();
         var fakeRepo = A.Fake<IGenericRepository<Balance>>();
@@ -27,7 +23,12 @@ public class BalanceServiceTests
             new Balance { Id = Guid.NewGuid(), AccountId = Guid.NewGuid(), Year = 2023, Month = 3, Amount = 200, Account = new Account { AccountName = "Canteen", AccountCode = "CT" } },
             new Balance { Id = Guid.NewGuid(), AccountId = Guid.NewGuid(), Year = 2023, Month = 4, Amount = 300, Account = new Account { AccountName = "R&D", AccountCode = "RD" } }
         };
-        A.CallTo(() => fakeRepo.GetAsync(null, null, A<Func<IQueryable<Balance>, IQueryable<Balance>>>._, false)).Returns(balances);
+        A.CallTo(() => fakeRepo.GetAsync(
+            A<System.Linq.Expressions.Expression<Func<Balance, bool>>>._,
+            A<Func<IQueryable<Balance>, IOrderedQueryable<Balance>>>._,
+            A<Func<IQueryable<Balance>, IIncludableQueryable<Balance, object>>>._, 
+            A<bool>._)).Returns(balances);
+
         A.CallTo(() => fakeUow.BalanceRepository).Returns(fakeRepo);
 
         var service = new BalanceService(fakeUow);
@@ -40,6 +41,5 @@ public class BalanceServiceTests
         result.Any(p => p.Month == 3 && p.Year == 2023).ShouldBeTrue();
         result.Any(p => p.Month == 4 && p.Year == 2023).ShouldBeTrue();
         result.First().Balances.Count.ShouldBeGreaterThan(0);
-        */
     }
 }
